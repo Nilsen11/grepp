7d969c67b65e9f0363af1ed154b5f8a15572cd7d
Nilsen11 <vansid99@gmail.com>
2019-01-13 03:33:49 +0200

    commit

--- grep.py
+++ grep.py
@@ -1,35 +1,49 @@
 import sys
 import re
 
+
+def read_file(_filename, encoding='utf-8'):
+    with open(_filename, encoding=encoding) as _file:
+        for _line in _file:
+            yield _line
+
+
+def check_line(_line, _pattern, _parameter):
+    if _pattern.search(_line):
+        return not _parameter
+    return _parameter
+
+
+def parsing_arguments():
+    if len(sys.argv) - 1 == 2:
+        _parameter = ''
+        _regular = sys.argv[1]
+        _filename = sys.argv[2]
+    elif len(sys.argv) - 1 == 3:
+        _parameter = sys.argv[1]
+        _regular = sys.argv[2]
+        _filename = sys.argv[3]
+    else:
+        raise Exception('the number of parameters must be 2 or 3')
+
+    if _parameter == '-v':
+        _parameter = True
+    elif _parameter == '':
+        _parameter = False
+    else:
+        raise Exception('invalid parameter')
+
+    return _parameter, _regular, _filename
+
+
 if __name__ == '__main__':
     try:
-        # check if 2 or 3 parameters
-        if len(sys.argv) - 1 == 2:
-            parameter = ''
-            # if first parameter equal -v  --> exception. (only for 3 param use)
-            if sys.argv[1] == '-v':
-                raise Exception('Invalid parameter')
-            else:
-                regular = re.compile(sys.argv[1])
-            filename = sys.argv[2]
-        elif len(sys.argv) - 1 == 3:
-            parameter = sys.argv[1]
-            regular = re.compile(sys.argv[2])
-            filename = sys.argv[3]
-        else:
-            raise Exception('the number of parameters must be 2 or 3')
+        parameter, regular, filename = parsing_arguments()
+        regular = re.compile(regular)
 
-        with open(filename) as file:
-            # satisfy the condition == ''  or not satisfy == '-v':
-            if parameter == '':
-                for line in file:
-                    if regular.findall(line):
-                        print(line)
-            elif parameter == '-v':
-                for line in file:
-                    if not regular.findall(line):
-                        print(line)
-            else:
-                raise Exception('Invalid parameter')
+        for line in read_file(filename):
+            if check_line(line, regular, parameter):
+                print(line)
+
     except Exception as e:
         print(type(e), e)
